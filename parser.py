6c501ba712c8f5affcddf0c87287ccef46b406d7
Oleg <oleg1babchenok1@gmail.com>
2023-08-02 16:22:28 +0300

    now working via requests to binance api

--- parser.py
+++ parser.py
@@ -1,50 +1,60 @@
 from telethon.sync import TelegramClient
 import os
-import asyncio
 from telethon import events
-import pyautogui as pag
+from credits import cookies, headers
+import requests
+import json
 from time import sleep
+
+
 api_id = os.getenv("api_id")
 api_hash = os.getenv("api_hash")
 phone_number = os.getenv("phone_number")
-client = TelegramClient(phone_number,api_id,api_hash)
+client = TelegramClient(phone_number, api_id, api_hash)
 last = ""
-@ client.on(events.NewMessage(pattern=r"[A-Za-z0-9]{8}"))
+unscсsful_atmps = 0
+
+
+@client.on(events.NewMessage(pattern=r"[A-Za-z0-9]{8}"))
 def message_handler(event):
-        code = event.message.to_dict()["message"]
-        if len(code) == 8 and last != code:
+    grab_flag = True
+    code = event.message.to_dict()["message"]
+    global last, unscсsful_atmps
+    if len(code) == 8 and last != code:
+        print(code)
+        last = code
+        data = json.dumps({"grabCode": code, "channel": "DEFAULT"})
+        response = requests.post(
+            'https://www.binance.com/bapi/pay/v1/private/binance-pay/gift-box/code/query',
+            cookies=cookies,
+            headers=headers,
+            data=data,
+        )
+        print(response.json())
+        sleep(0.1)
+        try:
+            grab_flag = response.json()["data"]["grabbed"]
+        except KeyError:
+            unscсsful_atmps += 1
+            return
+        if unscсsful_atmps == 8:
+            sleep(30)
+            unscсsful_atmps = 0
+        if not grab_flag:
 
-                print(code)
-                pag.moveTo(1528,599)
-                pag.click()
-                pag.click()
-                pag.hotkey("backspace")
-                pag.typewrite(code)
-                sleep(2)
-                pag.moveTo(1548, 718)
-                pag.click()
-                pag.click()
-                pag.click()
-                sleep(2.5)
-                pag.moveTo(1431, 796)
-                pag.click()
-                pag.click()
-                pag.moveTo(1471, 776)
-                pag.click()
-                pag.moveTo(1431, 857)
-                pag.click()
-                sleep(0.5)
-                pag.moveTo(1681, 231)
-                sleep(0.5)
-                pag.click()
-                pag.moveTo(1700, 231)
-                pag.click()
+            response = requests.post(
+                'https://www.binance.com/bapi/pay/v1/private/binance-pay/gift-box/code/grabV2',
+                cookies=cookies,
+                headers=headers,
+                data=data,
+            )
+            print(response.json())
+            print("C", code, "получено:", response.json()["data"]["grabAmountStr"], response.json()["data"]["currency"])
+            unscсsful_atmps = 0
+        else:
+            print(code, " уже забран!")
 
 
-
-
-
-
-
-client.start()
-client.run_until_disconnected()
+if __name__ == "__main__":
+    client.start()
+    client.run_until_disconnected()
